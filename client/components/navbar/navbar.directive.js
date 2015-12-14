6130fb43c492e0492398ab19dff50e71c1fe7b4c
wangzhiming <wangzhiming@didichuxing.com>
2015-12-14 12:31:02 +0800

    init

---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('colorApp')
+  .directive('navbar', function () {
+    return {
+      templateUrl: 'components/navbar/navbar.html',
+      restrict: 'E',
+      controller: 'NavbarCtrl'
+    };
+  });
